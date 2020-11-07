89f73229d4d21280d2dd1210f8a828e785ea1561
nialov <nikolasovaskainen@hotmail.com>
2020-11-07 16:18:37 +0200

    bumpversion

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Nikolas Ovaskainen"
 author = "Nikolas Ovaskainen"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.2"
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
