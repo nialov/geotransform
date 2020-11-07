f24c9c5de5eba857c0bc6e99afb25b96313d62c7
nialov <nikolasovaskainen@hotmail.com>
2020-11-07 23:04:28 +0200

    going for readthedocs

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from invoke import task
+import re
 
 patch = "patch"
 minor = "minor"
@@ -19,13 +20,14 @@ def make_tox(c):
 
 @task
 def make_pipenv_requirements(c):
-    c.run("pipenv run pipenv_to_requirements")
+    # Add -f for applications
+    c.run("pipenv run pipenv_to_requirements -f")
     c.run("pipenv run pipenv-setup sync --dev")
 
 
-@task
-def make_tox_docs(c):
-    c.run("pipenv run tox -e docs")
+# @task
+# def make_tox_docs(c):
+#     c.run("pipenv run tox -e docs")
 
 
 @task
@@ -41,17 +43,29 @@ def git_commit_all(c, commit_msg: str) -> int:
 
 @task(
     help={"patch_minor_major": "Patch, minor or major version bump."},
-    pre=[make_pytest, make_tox, make_pipenv_requirements, make_tox_docs],
-    post=[make_pytest],
+    pre=[make_pytest, make_tox, make_pipenv_requirements],
+    post=[make_pytest, make_dist],
 )
 def make_version_bump(c, patch_minor_major=patch):
     verify = input(
-        f"Are you sure you wish to do a {patch_minor_major} version bump? y/n"
+        f"Are you sure you wish to do a {patch_minor_major} version bump? y/n: "
     )
     if verify not in ["y", "yes", "Y"]:
         print(f"Aborting according to user input: {verify}")
         return
     git_commit_all(c, commit_msg="Git commit before version bump.")
     c.run(f"bump2version --verbose {patch_minor_major}")
-    c.run(make_dist_cmd)
-    git_commit_all(c, commit_msg="Git commit after version bump and make_dist.")
+
+
+@task
+def make_tagged_commit(c):
+    with open(".bumpversion.cfg") as cfg:
+        curr_version_line = [
+            line for line in cfg.readlines() if "current_version = " in line
+        ]
+        assert len(curr_version_line) == 1
+        curr_version_line: str = curr_version_line[0]
+        print(curr_version_line.strip())
+        pattern = re.compile("\d")
+        match = re.match(pattern, curr_version_line)
+        print(match)
